c500878bde963b14aa5009f48b44dd59dcea9c16
g4r7y <g4r7y@proton.me>
2026-01-17 01:57:04 +0000

    Make general improvements

--- ask.py
+++ ask.py
@@ -4,14 +4,17 @@ import os
 import sys
 import requests
 import json
+import argparse
+import readline
 
-GREEN = "\033[92m"
-YELLOW = "\033[33m"
-CYAN = "\033[36m"
-RED = "\033[31m"
-COL_END = "\033[0m"
+GREEN   = '\033[92m'
+YELLOW  = '\033[33m'
+CYAN    = '\033[36m'
+BR_CYAN = '\033[96m'
+RED     = '\033[31m'
+COL_END = '\033[0m'
 
-def intialiseCreds():
+def get_creds() -> dict[str,str]:
   cloudFlareAccount = os.environ.get('CLOUDFLARE_ACCOUNT')
   if cloudFlareAccount == None:
     print('You need to set CLOUDFLARE_ACCOUNT env var with your account ID.')
@@ -22,56 +25,81 @@ def intialiseCreds():
     print('You need to set CLOUDFLARE_API_TOKEN with your CloudFlare Workers AI API token.')
     exit(1)
 
-  creds = { "account": cloudFlareAccount, "token": apiToken }
+  creds = { 'account': cloudFlareAccount, 'token': apiToken }
   return creds
 
 
-def sendAiRequest(creds, messages):
-  # model = '@cf/meta/llama-3.2-1b-instruct'
-  model = '@cf/meta/llama-3.2-3b-instruct'
+def send_ai_request(creds: dict[str, str], messages: list[dict[str, str]]):
+  # model = '@cf/meta/llama-3.2-3b-instruct'
+  # model = '@cf/meta/llama-3.2-3b-instruct'
   # model = '@cf/meta/llama-3.1-8b-instruct'
+  model = '@cf/meta/llama-4-scout-17b-16e-instruct'
 
   payload = { 
     'messages': messages
   }
 
   urlTemplate = 'https://api.cloudflare.com/client/v4/accounts/{account}/ai/run/{model}'
-  url = urlTemplate.format(account=creds["account"], model=model)
-  hdrs = {'Authorization': 'Bearer '+ creds["token"]}
+  url = urlTemplate.format(account=creds['account'], model=model)
+  hdrs = {'Authorization': 'Bearer '+ creds['token']}
 
   try:
     response = requests.post(url, headers=hdrs, data=json.dumps(payload))
     response.raise_for_status()
-  except requests.exceptions.HTTPError as err:
-    print("Request failed.\n", err)
-    return ""
+  except Exception as err:
+    print('Request failed.\n', err)
+    return ''
   else:
-    # todo - validate response 
-    answer = response.json()['result']['response']
-    return answer
+    answer = response.json().get('result',{}).get('response')
+    if answer is not None:
+      return answer
+    print('Received unexpected response from Workers AI\n', response.json())
+    return ''
 
-def runChat():
-  creds = intialiseCreds()
-  systemMessage = { "role": "system", "content": "You are a helpful assistant called Bob. Please answer questions briefly. If you are not sure of the answer, just say you don't know. If the user prompt is 'more' or '>' then continue where your previous response was truncated." }
+def run_chat(prompt: str):
+  creds = get_creds()
+  systemMessage = { 'role': 'system', 'content': "You are a helpful assistant called Bob. Please answer questions briefly and professionally, without asking follow up questions. It is very important that you always end every response with '⏎'. If the user prompt is '>' then continue where your previous response was truncated." }
   conversation = []
   conversation.append(systemMessage)
 
-  prompt = ""
-  while (prompt.lower() != "exit" and prompt.lower() != "bye"):
-    prompt = input(GREEN + "Ask: " + YELLOW)
-    if (prompt.lower() == "clear"):
+  # if prompt provided just do a single prompt, otherwise do conversation mode
+  oneShot = prompt != ''
+  truncated = False
+
+  while True:
+    if truncated:
+      # ask for next part of truncated response
+      prompt = '>'
+    elif not oneShot:
+      # get new prompt from user
+      readline.parse_and_bind("tab: complete")
+      prompt = input(GREEN + 'Ask: ' + YELLOW)
+      
+    if (prompt.lower() == 'clear'):
       conversation = []
       conversation.append(systemMessage)
-      print(CYAN + "Conversation cleared." + COL_END + "\n")
+      print(BR_CYAN + 'Conversation cleared.' + COL_END + '\n')
     else:
-      conversation.append({ "role": "user", "content": prompt })
-      answer = sendAiRequest(creds, conversation)
-      print(CYAN + answer + COL_END + "\n")
-      conversation.append({ "role": "assistant", "content": answer })
+      conversation.append({ 'role': 'user', 'content': prompt })
+      answer = send_ai_request(creds, conversation)
+      print(BR_CYAN + answer + COL_END + '\n')
+      conversation.append({ 'role': 'assistant', 'content': answer })
+      # our system prompt asks to append special char when complete, so if it's not there then response truncated 
+      truncated = answer[-1] != '⏎'
+    if prompt.lower() == 'exit' or prompt.lower() == 'bye' or (oneShot and not truncated):
+      break
 
 
 try:
-  runChat()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('text', type=str, nargs='*', default=[])
+  args = parser.parse_args()
+  # take any command line text
+  prompt = ' '.join(args.text)
+  if not sys.stdin.isatty():
+    # append any piped stdin
+    prompt = prompt + ' ' + sys.stdin.read()
+  run_chat(prompt)
 except (KeyboardInterrupt, EOFError):
-  print(RED + "\nExiting..." + COL_END + "\n")
+  print(RED + '\nExiting...' + COL_END + '\n')
   sys.exit(0)
